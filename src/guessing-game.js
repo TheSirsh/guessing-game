ab8ceb19198f2919a16745c08a73871a6eed4c8c
TheSirsh <sirsh@bk.ru>
2022-08-15 16:44:49 +0300

    upd: the test is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
+
 class GuessingGame {
-    constructor() {}
-
+    constructor() { }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max-this.min)/2) + this.min
     }
 
     lower() {
-
+        return this.max = Math.round((this.max-this.min)/2) + this.min
     }
 
     greater() {
-
+        return this.min = Math.round((this.max-this.min)/2) + this.min
     }
 }
 
